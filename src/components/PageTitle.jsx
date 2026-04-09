aaf6e7d8fe2eb9b5c63b31a17ea2261b9295391b
robertnic9 <robertnicuta80@gmail.com>
2026-04-09 15:06:23 +0200

    Page title

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -3,8 +3,8 @@ import "@/styles/Pagetitle.css";
 
 export default function Pagetitle({ title }) {
   return (
-    <div className="w-full flex items-center justify-center sm:translate-y-14">
-      <h2 className="whitespace-nowrap uppercase outlineText">
+    <div className="w-full flex items-center justify-center overflow-hidden px-4">
+      <h2 className="whitespace-nowrap uppercase outlineText leading-none my-16">
         {title}
       </h2>
     </div>
